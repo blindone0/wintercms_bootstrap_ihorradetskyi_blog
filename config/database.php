a6930677fb5deade0e9f96e3216dcf3ca641668b
blindone0 <mrsettle1@gmail.com>
2021-05-31 00:43:47 +0300

    added blog

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'host'       => 'localhost',
             'port'       => 3306,
             'database'   => 'october',
-            'username'   => 'jkjarmfq_laravel',
+            'username'   => 'laravel',
             'password'   => 'laravel',
             'charset'    => 'utf8mb4',
             'collation'  => 'utf8mb4_unicode_ci',
